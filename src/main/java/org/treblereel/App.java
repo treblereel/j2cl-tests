f748f5e176eff39cca33f3238da4815c3ebdf943
Dmitrii Tikhomirov <chani@me.com>
2020-03-03 18:05:10 +0100

    the bug

--- src/main/java/org/treblereel/App.java
+++ src/main/java/org/treblereel/App.java
@@ -2,11 +2,13 @@ package org.treblereel;
 
 import elemental2.dom.DomGlobal;
 import org.gwtproject.core.client.EntryPoint;
+import org.gwtproject.i18n.client.NumberFormat;
 
 public class App implements EntryPoint {
 
     @Override
     public void onModuleLoad() {
-        DomGlobal.window.alert("RUN");
+        NumberFormat formatter = NumberFormat.getFormat("#,###");
+        DomGlobal.console.log(formatter.format(100));
     }
 }
